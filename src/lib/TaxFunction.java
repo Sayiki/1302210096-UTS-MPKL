846b5b64432e52ec3dceafb2aa20944ddd462bb6
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-21 00:56:33 +0700

    Switch Statements

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -19,30 +19,19 @@ public class TaxFunction {
     private static final double TAX_RATE = 0.05;
 
     public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-        int tax = 0;
-
         if (numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
-        }
-
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
         }
 
         int nontaxableIncome = SINGLE_PERSON_NONTAXABLE_INCOME;
         if (isMarried) {
             nontaxableIncome += MARRIED_NONTAXABLE_INCOME_ADDITION;
         }
-        nontaxableIncome += numberOfChildren * CHILD_NONTAXABLE_INCOME_ADDITION;
+        nontaxableIncome += Math.min(numberOfChildren, 3) * CHILD_NONTAXABLE_INCOME_ADDITION;
 
         int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
-        tax = (int) Math.round(TAX_RATE * (totalIncome - deductible - nontaxableIncome));
-
-        if (tax < 0) {
-            return 0;
-        } else {
-            return tax;
-        }
+        int tax = (int) Math.round(TAX_RATE * (totalIncome - deductible - nontaxableIncome));
+        return Math.max(tax, 0);
     }
 	
 }
