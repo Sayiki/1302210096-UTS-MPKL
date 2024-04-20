efd20a1a344e37081e231e511e7265ff289cc4bd
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-21 00:29:09 +0700

    Revert "Long Method code smell"
    This reverts commit 310b26a7c49857fea50bcf793fa506fd37104c7c.

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -19,6 +19,16 @@ public class TaxFunction {
     private static final double TAX_RATE = 0.05;
 
     public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        int tax = 0;
+
+        if (numberOfMonthWorking > 12) {
+            System.err.println("More than 12 month working per year");
+        }
+
+        if (numberOfChildren > 3) {
+            numberOfChildren = 3;
+        }
+
         int nontaxableIncome = SINGLE_PERSON_NONTAXABLE_INCOME;
         if (isMarried) {
             nontaxableIncome += MARRIED_NONTAXABLE_INCOME_ADDITION;
@@ -26,8 +36,13 @@ public class TaxFunction {
         nontaxableIncome += numberOfChildren * CHILD_NONTAXABLE_INCOME_ADDITION;
 
         int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
-        int taxableIncome = Math.max(totalIncome - deductible - nontaxableIncome, 0);
-        return (int) Math.round(TAX_RATE * taxableIncome);
+        tax = (int) Math.round(TAX_RATE * (totalIncome - deductible - nontaxableIncome));
+
+        if (tax < 0) {
+            return 0;
+        } else {
+            return tax;
+        }
     }
 	
 }
