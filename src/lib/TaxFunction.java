c5cf5d9e8dbd959f3ed002c0515b171751c90578
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-21 14:09:36 +0700

    delete comment

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -13,25 +13,28 @@ public class TaxFunction {
 	Rp 54.000.000 ditambah Rp 4.500.000 per anak hingga anak ketiga jika karyawan memiliki anak
 	 */
 	
-	private static final int SINGLE_PERSON_NONTAXABLE_INCOME = 54000000;
-    private static final int MARRIED_NONTAXABLE_INCOME_ADDITION = 4500000;
-    private static final int CHILD_NONTAXABLE_INCOME_ADDITION = 1500000;
-    private static final double TAX_RATE = 0.05;
-
-    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-        if (numberOfMonthWorking > 12) {
-            System.err.println("More than 12 month working per year");
-        }
-
-        int nontaxableIncome = SINGLE_PERSON_NONTAXABLE_INCOME;
-        if (isMarried) {
-            nontaxableIncome += MARRIED_NONTAXABLE_INCOME_ADDITION;
-        }
-        nontaxableIncome += Math.min(numberOfChildren, 3) * CHILD_NONTAXABLE_INCOME_ADDITION;
-
-        int totalIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
-        int tax = (int) Math.round(TAX_RATE * (totalIncome - deductible - nontaxableIncome));
-        return Math.max(tax, 0);
-    }
+     private static final int SINGLE_PERSON_NONTAXABLE_INCOME = 54000000;
+     private static final int MARRIED_NONTAXABLE_INCOME_ADDITION = 4500000;
+     private static final int CHILD_NONTAXABLE_INCOME_ADDITION = 1500000;
+     private static final double TAX_RATE = 0.05;
+ 
+     public static int calculateTax(int totalIncome, int deductible, int nontaxableIncome) {
+         int tax = (int) Math.round(TAX_RATE * (totalIncome - deductible - nontaxableIncome));
+         return Math.max(tax, 0);
+     }
+ 
+     public static int calculateNontaxableIncome(MaritalStatus maritalStatus, int numberOfChildren) {
+         int nontaxableIncome = SINGLE_PERSON_NONTAXABLE_INCOME;
+         if (maritalStatus == MaritalStatus.MARRIED) {
+             nontaxableIncome += MARRIED_NONTAXABLE_INCOME_ADDITION;
+         }
+         nontaxableIncome += Math.min(numberOfChildren, 3) * CHILD_NONTAXABLE_INCOME_ADDITION;
+         return nontaxableIncome;
+     }
+ 
+     public enum MaritalStatus {
+         SINGLE,
+         MARRIED
+     }
 	
 }
