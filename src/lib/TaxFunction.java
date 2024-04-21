3d010dbdec99e13c0c9728063af3746b2cd1b742
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-21 11:18:08 +0700

    Primitive Obsession Tax Function

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -18,13 +18,18 @@ public class TaxFunction {
     private static final int CHILD_NONTAXABLE_INCOME_ADDITION = 1500000;
     private static final double TAX_RATE = 0.05;
 
-    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+    enum MaritalStatus {
+        SINGLE,
+        MARRIED
+    }
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, MaritalStatus maritalStatus, int numberOfChildren) {
         if (numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
         }
 
         int nontaxableIncome = SINGLE_PERSON_NONTAXABLE_INCOME;
-        if (isMarried) {
+        if (maritalStatus == MaritalStatus.MARRIED) {
             nontaxableIncome += MARRIED_NONTAXABLE_INCOME_ADDITION;
         }
         nontaxableIncome += Math.min(numberOfChildren, 3) * CHILD_NONTAXABLE_INCOME_ADDITION;
