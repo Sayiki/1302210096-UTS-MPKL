59e9b15bcd725f98e383f52628aa7b2ea59ed21a
Arzaq Ajradika <arzaqajradika1@gmail.com>
2024-04-21 13:09:03 +0700

    Long Parameter List

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -8,98 +8,90 @@ import java.util.List;
 public class Employee {
 
 	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
-	
-	private int monthlySalary;
-	private int otherMonthlyIncome;
-	private int annualDeductible;
-	
-	private String spouseName;
-	private String spouseIdNumber;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+    private int yearJoined;
+    private int monthJoined;
+    private int dayJoined;
+    private boolean isForeigner;
+    private boolean gender; // true = Laki-laki, false = Perempuan
+    private int monthlySalary;
+    private int otherMonthlyIncome;
+    private int annualDeductible;
+    private String spouseName;
+    private String spouseIdNumber;
+    private String[] childNames;
+    private String[] childIdNumbers;
 
-	private List<String> childNames;
-	private List<String> childIdNumbers;
-	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
-	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
-	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}
-	}
-	
-	public void setAnnualDeductible(int deductible) {	
-		this.annualDeductible = deductible;
-	}
-	
-	public void setAdditionalIncome(int income) {	
-		this.otherMonthlyIncome = income;
-	}
-	
-	public void setSpouse(String spouseName, String spouseIdNumber) {
-		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
-	}
-	
-	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
-	
-	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-	}
+    public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.yearJoined = yearJoined;
+        this.monthJoined = monthJoined;
+        this.dayJoined = dayJoined;
+        this.isForeigner = isForeigner;
+        this.gender = gender;
+        this.childNames = new String[0];
+        this.childIdNumbers = new String[0];
+    }
+
+    public void setMonthlySalary(int grade) {
+        if (grade == 1) {
+            monthlySalary = 3000000;
+            if (isForeigner) {
+                monthlySalary = (int) (3000000 * 1.5);
+            }
+        } else if (grade == 2) {
+            monthlySalary = 5000000;
+            if (isForeigner) {
+                monthlySalary = (int) (3000000 * 1.5);
+            }
+        } else if (grade == 3) {
+            monthlySalary = 7000000;
+            if (isForeigner) {
+                monthlySalary = (int) (3000000 * 1.5);
+            }
+        }
+    }
+
+    public void setAnnualDeductible(int deductible) {
+        this.annualDeductible = deductible;
+    }
+
+    public void setAdditionalIncome(int income) {
+        this.otherMonthlyIncome = income;
+    }
+
+    public void setSpouse(String spouseName, String spouseIdNumber) {
+        this.spouseName = spouseName;
+        this.spouseIdNumber = idNumber;
+    }
+
+    public void addChild(String childName, String childIdNumber) {
+        this.childNames = addToArray(this.childNames, childName);
+        this.childIdNumbers = addToArray(this.childIdNumbers, childIdNumber);
+    }
+
+    public int getAnnualIncomeTax() {
+        // Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+        LocalDate date = LocalDate.now();
+        int monthWorkingInYear = (date.getYear() == yearJoined) ? date.getMonthValue() - monthJoined : 12;
+
+        TaxFunction.MaritalStatus maritalStatus = spouseIdNumber.isEmpty() ? TaxFunction.MaritalStatus.SINGLE : TaxFunction.MaritalStatus.MARRIED;
+        int nontaxableIncome = TaxFunction.calculateNontaxableIncome(maritalStatus, childIdNumbers.length);
+        int totalIncome = (monthlySalary + otherMonthlyIncome) * monthWorkingInYear;
+        return TaxFunction.calculateTax(totalIncome, annualDeductible, nontaxableIncome);
+    }
+
+    private static String[] addToArray(String[] array, String value) {
+        String[] newArray = new String[array.length + 1];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        newArray[array.length] = value;
+        return newArray;
+    }
 }
